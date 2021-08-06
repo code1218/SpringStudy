9dcb490aefaa7ac0607c75103c0c8d7f151322c0
junil <skjil1218@gmail.com>
2021-08-06 17:15:01 +0900

    JDBC 추가 커밋

--- mvcStudy(jun)/src/main/java/com/spring/mvc/controller/StudentController.java
+++ mvcStudy(jun)/src/main/java/com/spring/mvc/controller/StudentController.java
@@ -1,15 +1,20 @@
 package com.spring.mvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.spring.mvc.dao.StudentDAO;
 import com.spring.mvc.model.Student;
 
 @Controller
 public class StudentController {
+	
+	@Autowired
+	StudentDAO studentDAO;
 	
 	@RequestMapping(value = "/student/index", method = RequestMethod.GET)
 	public ModelAndView index() {
@@ -22,6 +27,10 @@ public class StudentController {
 	@RequestMapping(value = "/student/addStudent", method = RequestMethod.POST)
 	public ModelAndView addStudent(@ModelAttribute Student student) {
 		ModelAndView mav = new ModelAndView();
+		
+		//DB Insert
+		studentDAO.insert(student);
+		
 		mav.setViewName("/student/addStudent");
 		mav.addObject("student", student);
 		return mav;
---
+++ mvcStudy(jun)/src/main/java/com/spring/mvc/dao/StudentDAO.java
@@ -0,0 +1,32 @@
+package com.spring.mvc.dao;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import com.spring.mvc.model.Student;
+
+@Component
+public class StudentDAO {
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	public void setDataSource(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	/*
+	public List<Student> select(){
+		String sql = "select * from student_mst";
+		List<Student> students = jdbcTemplate.query(null, null)
+		return students;
+	}
+	*/
+	public void insert(Student student) {
+		String sql = "insert into student_mst values(0, ?, ?, ?)";
+		jdbcTemplate.update(sql, student.getId(), student.getName(), student.getPhone());
+	}
+}
