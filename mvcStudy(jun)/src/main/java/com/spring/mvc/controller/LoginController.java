d8a6347aadd057ca96c69c5ab68c6d854f86b306
junil <skjil1218@gmail.com>
2021-08-13 16:38:22 +0900

    16:38 commit

--- mvcStudy(jun)/src/main/java/com/spring/mvc/controller/LoginController.java
+++ mvcStudy(jun)/src/main/java/com/spring/mvc/controller/LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.spring.mvc.dao.study.LoginDAO;
+import com.spring.mvc.dao.LoginDAO;
 import com.spring.mvc.model.LoginModel;
 import com.spring.mvc.service.LoginService;
 
--- mvcStudy(jun)/src/main/java/com/spring/mvc/dao/LoginDAO.java
+++ mvcStudy(jun)/src/main/java/com/spring/mvc/dao/LoginDAO.java
@@ -1,9 +1,13 @@
-package com.spring.mvc.dao.study;
+package com.spring.mvc.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.spring.mvc.model.LoginModel;
@@ -20,8 +24,21 @@ public class LoginDAO {
 	
 	public int loginCheck(LoginModel loginModel) {
 		String sql = "select count(*) from user_mst where user_id = ? and user_pwd = ?";
-		int flag = jdbcTemplate.queryForObject(sql, Integer.class, loginModel.getLogin_id(), loginModel.getLogin_pwd());
+		int flag = jdbcTemplate.queryForObject(sql, new Object[] {loginModel.getLogin_id(), loginModel.getLogin_pwd()},
+				new RowMapper<Integer>() {
+			@Override
+			public Integer mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return rs.getInt(1);
+			}
+		});
 		return flag;
 	}
 
 }
+
+
+
+
+
+
+
